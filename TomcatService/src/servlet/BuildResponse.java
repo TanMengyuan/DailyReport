852cde81a772b566f1722586a940c8f9108edf5d
mengyuan.tan <tanmy1016@outlook.com>
2020-02-23 13:07:33 +0800

    v1.2

--- TomcatService/src/servlet/BuildResponse.java
+++ TomcatService/src/servlet/BuildResponse.java
@@ -7,16 +7,16 @@ class BuildResponse {
     StringBuilder buildResponse(String date, List<List<String>> lists) {
         StringBuilder result = new StringBuilder();
         result.append("<html><body><table border=\"1\" align=\"center\" " +
-                "cellspacing=\"0\" width=\"85%\" style=\"table-layout: fixed\">");
+                "cellspacing=\"0\" style=\"table-layout: fixed\">");
         result.append(String.format("<tr><h3 align=\"center\">%s 汇总结果</h3></tr>",
                 date));
         result.append("<tr>" +
-                "<th>姓名</th>" +
-                "<th>是否有发热症状</th>" +
-                "<th>是否与疫区人员接触</th>" +
-                "<th>工作简报</th>" +
-                "<th>其他</th>" +
-                "<th>提交（修改）时间</th>" +
+                "<th width=\"5%\">姓名</th>" +
+                "<th width=\"8%\">是否有发热症状</th>" +
+                "<th width=\"8%\">是否与疫区人员接触</th>" +
+                "<th width=\"15%\">工作简报</th>" +
+                "<th width=\"15%\">其他</th>" +
+                "<th width=\"8%\">提交（修改）时间</th>" +
                 "</tr>");
         for (List<String> line: lists) {
             result.append("<tr>");
@@ -27,7 +27,8 @@ class BuildResponse {
                         content = "<font color=red>" + content + "</font>";
                     }
                 }
-                result.append("<td style=\"word-wrap: break-word\">").append(content).append("</td>");
+                result.append("<td style=\"word-wrap: break-word\">")
+                        .append(content).append("</td>");
             }
             result.append("</tr>");
         }
