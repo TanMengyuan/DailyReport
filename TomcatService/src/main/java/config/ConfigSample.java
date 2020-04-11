4be57042ae48623d504271a8894c5fe516175f7a
mengyuan.tan <tanmy1016@outlook.com>
2020-04-11 22:16:34 +0800

    v1.5.2

--- TomcatService/src/main/java/config/ConfigSample.java
+++ TomcatService/src/main/java/config/ConfigSample.java
@@ -16,4 +16,8 @@ public class ConfigSample {
                     "aaa", "bbb", "ccc", "ddd"
             );
     public static String startTime = "2020-04-09";
+    public static int shownScheduleTimes = 2;
+    public static String loginUser = "admin_user_name";
+    public static String loginPassword = "admin_user_password";
+    private static String loginScript = "/init?user=admin_user_name&password=admin_user_password";
 }
